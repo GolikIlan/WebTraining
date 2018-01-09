0cc44cdc22bee852934307705149149dc57eeab2
Ilan Golik <alexg@codevalue.net>
2018-01-09 21:36:34 +0200

    separated to different modules

--- myshop with modules/src/app/app.module.ts
+++ myshop with modules/src/app/app.module.ts
@@ -8,12 +8,11 @@ import { RouterModule, Routes } from "@angular/router";
 import { SaveDialogComponent } from '../shared/save-dialog/save-dialog.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppComponent } from './app.component';
-import { AppRoutingModule, APP_ROUTING_COMPONENTS } from './app.routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { MainLayoutModule } from '../layout/main-layout.module';
 import { MainLayoutComponent } from '../layout/main-layout/main-layout.component';
 import { ProductsModule } from '../products/products.module';
 import { CoreModule } from '../core/core.module';
-//import { CartComponent } from '../cart/cart/cart.component';
 
 
 @NgModule({
@@ -36,54 +35,3 @@ import { CoreModule } from '../core/core.module';
 })
 export class AppModule { 
 }
-
-
-    /*CategoriesDataService, 
-    ProductsDataService, 
-    SelectionStateService, 
-    MenuItemsProvider,
-     CartManagementService, 
-     LocalizationService, 
-     LOCALIZATION_PROVIDERS, 
-     TitleCasePipe,
-     CREDENTIALS_PROVIDERS,
-     LoginSevice,
-     PERMISSIONS_PROVIDERS,
-     PermissionServise,
-     UserPermissionsStatusProvider,
-     NavigationManagerService,
-     AuthGuard, 
-     HasPermissionGuard,
-     CanExitNotSavedRouteGuard,
-     ShowOnClickDialogProviderDirective,*/
-
-
-    /*AppComponent,
-    HomeComponent, 
-    AboutComponent, 
-    ContactsComponent,
-    LogoutComponent,
-    PageNotFoundComponent,
-    MenuComponent,
-    ProductsComponent,
-    HeaderComponent,
-    ProductComponent,
-    ClosebuttonComponent,
-    ProductdetailsComponent,
-    AnchorwrapperComponent,
-    CartComponent,
-    CartaddingwrapperComponent,
-    CartlineComponent,
-    EndoflinebuttonComponent,
-    LocalizationPipe,
-    LoginComponent,
-    NumberValidationDirective,
-    TargetDirective,
-    AddNewProductComponent,
-    SaveDialogComponent,
-    SaveOnClickDialogProviderDirective,
-    SendMessageComponent,
-    SendOnClickDialogProviderDirective,
-    ShowOnClickDialogProviderDirective,
-    OnLoadListenerDirective,
-    CartDetailsComponent,*/
